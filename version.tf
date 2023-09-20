68115556eb3a190a167984ce6175d5291929510c
dafinours~ <kenneprosper@gmail.com>
2023-09-20 12:18:01 -0500

    changed version

--- version.tf
+++ version.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
  aws = {
  source = "hashicorp/aws" 
- version = "4.61.0"  
+ version = "5.17.0"  
   }
   }
 }
 provider "aws" {
   region = "us-east-1"
-}
+}git 
